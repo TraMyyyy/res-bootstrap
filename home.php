40cf0e95b63fa5ec36d86c7e8db5ac296379487f
TraMyyyy <ttmy2061@gmail.com>
2021-09-25 01:28:05 +0700

    add home screen

---
+++ home.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style-home.css">
+  </head>
+  <body>
+
+    <!-- Navbar here -->
+<nav class=" navbar navbar-expand-lg navbar-light ">
+   <div class ="container">
+    <div class ="col-6">
+      <a class="navbar-brand" href="">
+        <img src="images/logo.png" alt="" >
+      </a>
+    </div>
+    <div class ="col-3">
+      <ul class="navbar-nav navbar-collapse me-auto mb-2 mb-lg-0 p-3 ">
+        <li class="nav-item">
+          <a class="nav-link " href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Categories</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Food</a>
+        </li>
+        <li class ="nav-item">
+            <a class ="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+   </div>  
+</nav>
+
+    <!-- Navbar end here -->
+
+    <!-- Search start here -->
+<nav class="navbar">
+  <div class="container-fluid search justify-content-center">
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search for Foods..." aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    <!-- Search end here -->
+
+<!-- Explore food start here -->
+
+  <div class = "text-center fs-1 p-4 fw-bold ">
+    Explore Foods
+  </div>
+<div class ="card-group m-4"> 
+    <a href="#"class = "card" style ="width:20%">
+      <img src="images/pizza.jpg" class ="card-img" alt="#">
+       <div class = "card-img-overlay">
+         <h1 class ="card-footer text-center">Pizza</h1>
+        </div>
+    </a>
+    <a href="#"class = "card"  style ="width:20%">
+      <img src="images/burger.jpg" class ="card-img" alt="#">
+       <div class = "card-img-overlay">
+         <h1 class ="card-footer text-center">Burger</h1>
+      </div>
+    </a>
+    <a href="#"class = "card" style ="width:20%">
+      <img src="images/momo.jpg" class ="card-img" alt="#">
+       <div class = "card-img-overlay">
+         <h1 class ="card-footer text-center">Momo</h1>
+      </div>
+    </a>
+</div>
+<!-- Explore food end here -->
+
+<!-- Food menu start here -->
+<div class = "menu bg-light">
+  <div class ="text-center fs-1 p-4 fw-bold">
+    Food Menu
+  </div>
+
+</div>
+<!-- Food menu end here -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+</body>
+</html>
---
+++ manage-order.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>Manage order
+    </title>
+  </head>
+  <body>
+      <div class = "container-fluid">
+  <ul class="nav justify-content-center p-3 me-auto mb-2 mb-lg-0 ">
+  <li class="nav-item">
+    <a class="nav-link" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Admin</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Categories</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Food</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Order</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Logout</a>
+  </li>
+</div>
+</ul>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
